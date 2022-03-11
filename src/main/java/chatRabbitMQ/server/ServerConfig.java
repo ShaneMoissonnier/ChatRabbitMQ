cc565eaa89e5c731250509d2a182b4191d8ce8af
Esdia <theo.hermitte@protonmail.com>
2022-03-11 15:02:04 +0100

    Added some logging

--- src/main/java/chatRabbitMQ/server/ServerConfig.java
+++ src/main/java/chatRabbitMQ/server/ServerConfig.java
@@ -7,38 +7,58 @@ import com.rabbitmq.client.ConnectionFactory;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
+import java.util.logging.Logger;
 
-public class ServerConfig {
+/**
+ * This class's purpose is to configure the Rabbit-MQ Server by declaring the necessary exchanges.
+ * <p>
+ * The class {@link ServerClean} is used to remove these exchanges from the server.
+ */
+class ServerConfig {
     private static final String HOST = "localhost";
     private static final String EXCHANGE_MESSAGES_NAME = "messages";
     private static final String EXCHANGE_STATUS_NAME = "status";
 
+    private final Logger logger;
 
     private Connection connection;
     private Channel channel;
 
     private void connect() throws IOException, TimeoutException {
+        logger.info("Connecting to RabbitMQ-Server...");
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost(HOST);
 
         this.connection = factory.newConnection();
         this.channel = connection.createChannel();
+        logger.info("Connection successful");
     }
 
     private void createExchanges() throws IOException {
+        logger.info("Declaring exchanges...");
         this.channel.exchangeDeclare(EXCHANGE_MESSAGES_NAME, BuiltinExchangeType.FANOUT);
+        logger.info("   - '" + EXCHANGE_MESSAGES_NAME + "' exchange declared");
         this.channel.exchangeDeclare(EXCHANGE_STATUS_NAME, BuiltinExchangeType.FANOUT);
+        logger.info("   - '" + EXCHANGE_STATUS_NAME + "' exchange declared");
+        logger.info("Exchanges declaration done");
     }
 
     private void disconnect() throws IOException, TimeoutException {
+        logger.info("Disconnecting from RabbitMQ-Server...");
         this.channel.close();
         this.connection.close();
+        logger.info("Disconnection successful");
     }
 
     private ServerConfig() throws IOException, TimeoutException {
+        System.setProperty("java.util.logging.SimpleFormatter.format", "%4$s: %5$s%n");
+        logger = Logger.getLogger("serverConfig");
+
+        logger.info("Starting server configuration...");
         this.connect();
         this.createExchanges();
         this.disconnect();
+        logger.info("Successfully configured RabbitMQ-Server !");
     }
 
     public static void main(String[] args) throws IOException, TimeoutException {
